e249e2eea0cf4fff56f989ad57803c3fb14d7dcc
Nikita Naumenko <naumenkoniki@gmail.com>
2020-12-30 20:18:51 +0300

    fix db

--- knexfile.js
+++ knexfile.js
@@ -8,29 +8,21 @@ const migrations = {
 
 module.exports = {
   development: {
-    client: 'pg',
+    client: 'sqlite3',
     connection: {
-      host: '127.0.0.1',
-      user: 'postgres',
-      password: 'postgres',
-      database: 'project-lvl-4-dev',
+      filename: './database.sqlite',
     },
     useNullAsDefault: true,
     migrations,
   },
   test: {
-    client: 'pg',
-    connection: {
-      host: '127.0.0.1',
-      user: 'postgres',
-      password: 'postgres',
-      database: 'project-lvl-4-test',
-    },
+    client: 'sqlite3',
+    connection: ':memory:',
     useNullAsDefault: true,
     migrations,
   },
   production: {
-    client: 'pg',
+    client: 'sqlite3',
     connection: {
       filename: './database.sqlite',
     },
@@ -38,3 +30,35 @@ module.exports = {
     migrations,
   },
 };
+// module.exports = {
+//   development: {
+//     client: 'pg',
+//     connection: {
+//       host: '127.0.0.1',
+//       user: 'postgres',
+//       password: 'postgres',
+//       database: 'project-lvl-4-dev',
+//     },
+//     useNullAsDefault: true,
+//     migrations,
+//   },
+//   test: {
+//     client: 'pg',
+//     connection: {
+//       host: '127.0.0.1',
+//       user: 'postgres',
+//       password: 'postgres',
+//       database: 'project-lvl-4-test',
+//     },
+//     useNullAsDefault: true,
+//     migrations,
+//   },
+//   production: {
+//     client: 'pg',
+//     connection: {
+//       filename: './database.sqlite',
+//     },
+//     useNullAsDefault: true,
+//     migrations,
+//   },
+// };
