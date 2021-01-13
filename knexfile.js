6a02a6527bfce71428b70400872ca57c08c7a821
Nikita Naumenko <naumenkoniki@gmail.com>
2021-01-13 17:54:52 +0300

    fix connection

--- knexfile.js
+++ knexfile.js
@@ -28,8 +28,14 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    ssl: true,
-    connection: process.env.DATABASE_URL,
+    // connection: process.env.DATABASE_URL,
+    connection: {
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+    },
     useNullAsDefault: true,
     migrations,
   },
