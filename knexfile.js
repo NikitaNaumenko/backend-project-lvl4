6e40da96ff2c7d97866b90d31b75fe517d55e845
Nikita Naumenko <naumenkoniki@gmail.com>
2021-01-13 19:41:33 +0300

    fix ssl

--- knexfile.js
+++ knexfile.js
@@ -34,6 +34,7 @@ module.exports = {
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
+      ssl: true,
     },
     useNullAsDefault: true,
     migrations,
