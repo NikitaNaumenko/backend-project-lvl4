8fecd9e678f6e5db894ddd3e63cf182a37a4f028
Nikita Naumenko <naumenkoniki@gmail.com>
2021-01-13 18:05:06 +0300

    knex

--- knexfile.js
+++ knexfile.js
@@ -35,7 +35,7 @@ module.exports = {
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
-      ssl: false
+      ssl: true
     },
     useNullAsDefault: true,
     migrations,
