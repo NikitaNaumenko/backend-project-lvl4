cfd6f2f467f475210893b04a3fdc869533ab0fc1
Nikita Naumenko <naumenkoniki@gmail.com>
2021-02-07 11:36:41 +0300

    add edit

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -1,18 +1,22 @@
 import getApp from '../server/index.js';
+import { getTestData, prepareData } from './helpers/index.js';
 
 describe('test users CRUD', () => {
   let app;
   let knex;
   let models;
+  let testData;
 
   beforeAll(async () => {
     app = await getApp();
     knex = app.objection.knex;
     models = app.objection.models;
-  })
+    testData = getTestData();
+  });
 
   beforeEach(async () => {
     await knex.migrate.latest();
+    prepareData(app);
   });
 
   it('index', async () => {
@@ -28,6 +32,16 @@ describe('test users CRUD', () => {
     const res = await app.inject({
       method: 'GET',
       url: app.reverse('newUser'),
+    });
+
+    expect(res.statusCode).toBe(200);
+  });
+
+  it('edit', async () => {
+    const user = await models.user.query().findOne({ email: testData.users.existing.email });
+    const res = await app.inject({
+      method: 'GET',
+      url: app.reverse('editUser', { id: user.id.toString() }),
     });
 
     expect(res.statusCode).toBe(200);
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,7 @@
 // @ts-check
 
+import i18next from 'i18next';
+
 export default (app) => {
   app
     .get('/users', { name: 'users' }, async (req, reply) => {
@@ -10,7 +12,23 @@ export default (app) => {
       const user = await new app.objection.models.user();
       reply.render('users/new', { user });
     })
-    .post('/users', async (req, reply) => {
+    .get('/users/:id/edit', { name: 'editUser' }, async (req, reply) => {
+      const { id } = req.params;
+      const user = await app.objection.models.user.query().findById(id);
 
+      reply.render('users/edit', { user });
+    })
+    .post('/users', async (req, reply) => {
+      try {
+        const user = await app.objection.models.user.fromJson(req.body.data);
+        await app.objection.models.user.query().insert(user);
+        req.flash('info', i18next.t('flash.users.create.success'));
+        reply.redirect(app.reverse('root'));
+        return reply;
+      } catch ({ data }) {
+        req.flash('error', i18next.t('flash.users.create.error'));
+        reply.render('users/new', { user: req.body.data, errors: data });
+        return reply;
+      }
     });
 };
