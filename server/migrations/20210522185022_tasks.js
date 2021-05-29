22320cf09a80338d7c1b145bf8dd056bedf0ac76
Nikita Naumenko <naumenkoniki@gmail.com>
2021-05-29 17:51:31 +0300

    fix

--- server/migrations/20210522185022_tasks.js
+++ server/migrations/20210522185022_tasks.js
@@ -1,15 +1,15 @@
 exports.up = function(knex) {
    return knex.schema.createTable('tasks', (table) => {
     table.increments('id').primary();
-    // table.integer('creator_id').notNullable();
-    // table.integer('status_id').notNullable();
-    // table.integer('executor_id');
+    table.integer('creator_id').notNullable();
+    table.integer('status_id').notNullable();
+    table.integer('executor_id');
     table.string('name');
     table.string('description');
 
-    table.integer('creator_id').references('id').inTable('users');
-    table.integer('status_id').references('id').inTable('statuses');
-    table.integer('executor_id').references('id').inTable('users')
+    table.foreign('creator_id').references('id').inTable('users');
+    table.foreign('status_id').references('id').inTable('statuses');
+    table.foreign('executor_id').references('id').inTable('users')
   })
 };
 
--- server/migrations/20210526092136_task_labels.js
+++ server/migrations/20210526092136_task_labels.js
@@ -4,8 +4,8 @@ exports.up = function(knex) {
     table.integer('task_id').notNullable();
     table.integer('label_id').notNullable();
 
-    table.foreign('task_id').references('id').inTable('tasks').onDelete('RESTRICT');
-    table.foreign('label_id').references('id').inTable('labels').onDelete('RESTRICT');
+    table.foreign('task_id').references('id').inTable('tasks');
+    table.foreign('label_id').references('id').inTable('labels');
   })
 };
 
